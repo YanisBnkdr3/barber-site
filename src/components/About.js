d309c20503bded2fd52f07b58bc0a9777d4a3de7
YanisBnkdr3 <yanisbnkdr76@gmail.com>
2026-05-26 20:29:38 -0400

    updating About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,5 @@
 import { motion } from "framer-motion";
-import img1 from "../images/barber1.png";
-import img2 from "../images/barber2.png";
-import img3 from "../images/barber3.png";
+import img1 from "../images/logo2.png";
 
 export default function About({ t }) {
   return (
@@ -20,64 +18,35 @@ export default function About({ t }) {
           display: "flex",
           flexWrap: "wrap",
           alignItems: "center",
+          justifyContent: "center",
           gap: "40px",
         }}
       >
-        {/* 🔥 IMAGES */}
-        <div style={{ display: "flex", gap: "15px", flexWrap: "wrap" }}>
-          <motion.img
-            src={img1}
-            alt="barber1"
-            initial={{ opacity: 0, x: -50 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1 }}
-            style={{
-              width: "260px",
-              height: "320px",
-              objectFit: "cover",
-              borderRadius: "10px",
-            }}
-          />
-
-          <div
-            style={{ display: "flex", flexDirection: "column", gap: "15px" }}
-          >
-            <motion.img
-              src={img2}
-              alt="barber2"
-              initial={{ opacity: 0, x: -30 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.3 }}
-              style={{
-                width: "150px",
-                height: "150px",
-                objectFit: "cover",
-                borderRadius: "10px",
-              }}
-            />
-
-            <motion.img
-              src={img3}
-              alt="barber3"
-              initial={{ opacity: 0, x: -30 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{ delay: 0.5 }}
-              style={{
-                width: "150px",
-                height: "150px",
-                objectFit: "cover",
-                borderRadius: "10px",
-              }}
-            />
-          </div>
-        </div>
+        {/* 🔥 IMAGE UNIQUE */}
+        <motion.img
+          src={img1}
+          alt="barber"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+          style={{
+            width: "100%",
+            maxWidth: "400px",
+            height: "auto",
+            borderRadius: "15px",
+            objectFit: "cover",
+          }}
+        />
 
         {/* 📝 TEXTE */}
         <motion.div
           initial={{ opacity: 0, x: 50 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 1 }}
-          style={{ maxWidth: "500px" }}
+          style={{
+            maxWidth: "500px",
+            textAlign: "center",
+          }}
         >
           <h2 style={{ color: "#d4af37" }}>{t.about.title}</h2>
 
@@ -92,6 +61,7 @@ export default function About({ t }) {
           <p style={{ lineHeight: "1.6", marginTop: "10px" }}>
             {t.about.text3}
           </p>
+
           <p style={{ lineHeight: "1.6", marginTop: "10px" }}>
             {t.about.text4}
           </p>
